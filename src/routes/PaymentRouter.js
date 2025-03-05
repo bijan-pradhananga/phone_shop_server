d8481994ea083344e4c3e23bd43cd7ba83006f48
Bijan <bijan2059@gmail.com>
2025-03-05 14:40:43 +0545

    add payment routes

--- src/routes/PaymentRouter.js
+++ src/routes/PaymentRouter.js
@@ -3,7 +3,7 @@ const Payment = require('../controllers/PaymentController');
 const pInstance = new Payment()
 const paymentRouter = express.Router()
 
-// paymentRouter.get('/complete-payment', pInstance.completePayment);
-// paymentRouter.get('/complete-payment2', pInstance.completeKhaltiPayment);
+paymentRouter.get('/complete-payment', pInstance.completePayment);
+paymentRouter.get('/complete-payment2', pInstance.completeKhaltiPayment);
 
 module.exports =  paymentRouter;
