66c9bbcb17996fe462e2ae4b2ecce52bc423481f
Bijan <bijan2059@gmail.com>
2025-06-22 18:19:43 +0545

    added routes

--- src/routes/PaymentRouter.js
+++ src/routes/PaymentRouter.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express');
 const Payment = require('../controllers/PaymentController');
-const pInstance = new Payment()
-const paymentRouter = express.Router()
+const pInstance = new Payment();
+const paymentRouter = express.Router();
 
 paymentRouter.get('/complete-payment', pInstance.completePayment);
 paymentRouter.get('/complete-payment2', pInstance.completeKhaltiPayment);
+paymentRouter.get('/stats', pInstance.getPaymentStats);
+paymentRouter.get('/by-date', pInstance.getPaymentsByDate);
 
-module.exports =  paymentRouter;
+module.exports = paymentRouter;
